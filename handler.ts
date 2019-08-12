6dd728380ddcaeaaf3168cbc8db7c8e5e8524635
Cheston Lee <clee@corybooker.com>
2019-08-12 10:17:04 -0700

    Initial commit, nothing working just yet. Mostly setup.

---
+++ handler.ts
@@ -0,0 +1,20 @@
+import { Handler, Context, Callback } from 'aws-lambda';
+
+interface HelloResponse {
+  statusCode: number;
+  body: string;
+}
+
+const hello: Handler = async (event: any, context: Context, callback: Callback) => {
+  const response: HelloResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello ${Math.floor(Math.random() * 10)}`
+    })
+  };
+
+  // callback(undefined, response);
+  return response;
+};
+
+export { hello };
---
+++ src/auth.ts
@@ -0,0 +1,6 @@
+const oauthLogin = function(consumerKey: String, consumerSecret: String) {
+  console.debug('Authenticated with Twittter');
+};
+const batchDelete = function() {};
+
+export { oauthLogin, batchDelete }
